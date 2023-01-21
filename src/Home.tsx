86add3336dc2c37ca41ea3cc42e28ebb45cfbf51
Cauê Almeida <nncl@live.com>
2023-01-20 22:56:30 -0300

    feat(pages): Creating form

--- src/Home.tsx
+++ src/Home.tsx
@@ -17,15 +17,17 @@ export function Home() {
         Sexta, 4 de Novembro de 2022.
       </Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        placeholderTextColor="#6B6B6B"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          placeholderTextColor="#6B6B6B"
+        />
 
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
