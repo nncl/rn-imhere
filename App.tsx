b9cde7a40a0bcadf5c1e099bcacf69a6386e7648
Cauê Almeida <nncl@live.com>
2023-02-15 20:41:10 -0300

    style: Formating tags

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
-      <Home/></>
+      <Home/>
+    </>
   );
 }
 
